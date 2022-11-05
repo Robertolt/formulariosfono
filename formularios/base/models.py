fd49498f04dbcaea2514c8788706949bd9372ab8
robertolt <rl.beto.lorenzoni@gmail.com>
2022-11-04 22:45:51 -0300

    feito admin com alguns erros

--- formularios/base/models.py
+++ formularios/base/models.py
@@ -16,15 +16,8 @@ class UserManager(BaseUserManager):
         if not username:
             raise ValueError("The given username must be set")
         email = self.normalize_email(email)
-        # Lookup the real model class from the global app registry so this
-        # manager method can be used in migrations. This is fine because
-        # managers are by definition working on the real model.
-        GlobalUserModel = apps.get_model(
-            self.model._meta.app_label, self.model._meta.object_name
-        )
-        username = GlobalUserModel.normalize_username(username)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.password = make_password(password)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
